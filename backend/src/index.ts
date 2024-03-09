4591561a6a9fefe3e773c076fdbf0ed6b3b4cbd5
sarah-mosbah <sarahmosbah101@gmail.com>
2024-03-09 22:26:53 +0200

    Backend starter

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello there");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("app is listening"));
