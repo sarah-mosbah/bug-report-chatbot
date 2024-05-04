4d38d38e4213df3b35751c55d6fea0649f680a06
sarah-mosbah <sarahmosbah101@gmail.com>
2024-05-04 14:32:12 +0300

    Conversation

--- frontend/chat-bot/src/app/components/chat-bot/chat-bot.component.ts
+++ frontend/chat-bot/src/app/components/chat-bot/chat-bot.component.ts
@@ -1,8 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  inject,
+  signal,
+} from '@angular/core';
 import { map } from 'rxjs';
 import { ChatBotService } from 'src/app/services/chatbot.service';
 
+export interface Message {
+  text: string;
+  sender: 'bot' | 'user';
+}
 @Component({
   selector: 'app-chat-bot',
   templateUrl: './chat-bot.component.html',
@@ -12,15 +22,33 @@ import { ChatBotService } from 'src/app/services/chatbot.service';
   providers: [ChatBotService],
 })
 export class ChatBotComponent {
-  response = signal<string | undefined>(undefined);
+  @ViewChild('input') input!: ElementRef;
+
+  currentTime = new Date();
+  messages = signal<Message[]>([]);
   chatBotService = inject(ChatBotService);
 
   sendChatBotMessage(message: string): void {
+    this.currentTime = new Date();
+
+    this.addNewMessageToConversation(message, 'user');
+    this.input.nativeElement.value = '';
+
     this.chatBotService
       .sendMessage$(message)
       .pipe(map((chatbotResponse) => chatbotResponse.fulfillmentText))
       .subscribe((responseMessage) => {
-        this.response.set(responseMessage);
+        this.addNewMessageToConversation(responseMessage, 'bot');
       });
+  }
+
+  private addNewMessageToConversation(
+    message: string,
+    sender: 'user' | 'bot'
+  ): void {
+    this.messages.update((messages) => [
+      ...messages,
+      { text: message, sender },
+    ]);
   }
 }
